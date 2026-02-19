d39bb718e643835d55988535dfdc221632bd520a
Hassanmahmood4 <hmahmood19662004@gmail.com>
2026-02-19 20:41:01 +0500

    Set up Django backend with tasks API and admin

---
+++ users/views.py
@@ -0,0 +1,23 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
+from .serializers import TaskSerializer
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def task_list_create(request):
+    ...
+def task_list_create(request):
+    if request.method == 'GET':
+        tasks = Task.objects.all().order_by('-id')
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
